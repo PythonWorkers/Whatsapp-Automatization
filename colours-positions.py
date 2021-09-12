e939ff0f10ab51a24f5bc8b1daacb97fc57fa9a8
canerceylan98 <90556576+canerceylan98@users.noreply.github.com>
2021-09-12 14:45:53 +0300

    Delete colours-positions.py

--- colours-positions.py
+++
@@ -1,9 +0,0 @@
-import pyautogui as pt
-from time import sleep
-
-while True:
-    posXY = pt.position()
-    print(posXY, pt.pixel(posXY[0], posXY[1]))
-    sleep(2)
-    if posXY[0] == 0:
-        break
